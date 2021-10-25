7ff314ea1740a8bea107058bf9aedbf0a99c8fd4
LeNgocMinhAnh <minhanh15061999@gmail.com>
2021-10-25 23:09:16 +0700

    change code from admin page to place admin page

--- pages/admin/place.js
+++ pages/admin/place.js
@@ -1,219 +1,61 @@
+import NavBar from "../../components/navbar/Navbar";
+import AddPlace from "../../components/tab/addPlace";
 import { getPlace } from "../../services/api";
+import { useDebounce } from "../../services/hooks";
 import PlaceForm from "../../components/admin/place-form";
-import { useEffect, useState, useCallback } from "react";
+import Sidebar from "../../components/admin/side-bar";
+import { useEffect, useState } from "react";
 
-export async function getServerSideProps(context) {
-    const { data } = await getPlace();
-    console.log("data");
-    return {
-      props: {
-        places: data.results,
-      },
-    };
+export default function PlaceManage({ user }) {
+  const [places, setPlaces] = useState([]);
+  const [total, setTotal] = useState(0);
+
+  const [searchText, setSearchText] = useState("");
+  const debouncedValue = useDebounce(searchText, 300);
+
+  useEffect(() => {
+    if (!!user) {
+      const data = { search: debouncedValue };
+      getPlace(data).then(({ data, status }) => {
+        if (status === 200) {
+          setPlaces(data.results);
+          setTotal(data.total);
+        }
+      });
+    }
+  }, [user, debouncedValue]);
+
+  if (user == undefined) {
+    return null;
   }
-
-export default function AddPlace({ places }) {
-
-  const [isShowPlaceForm, setShowPlaceForm] = useState(true);
-  const isPlaceFormShow = useCallback(() => {
-    setShowPlaceForm(!isShowPlaceForm);
-  }, [isShowPlaceForm]);
-  
-
+  if (user === null) {
+    return (
+      <Link
+        href={`/user/login?redirect_uri=${router.asPath || router.pathname}`}
+      >
+        <button className="p-2 border-2 rounded-full shadow-2xl text-LightSeaGreen bg-xanhlo border-GhostWhi">
+          Đăng nhập
+        </button>
+      </Link>
+    );
+  }
   return (
-    <div className="bg-gray-50 flex-grow py-12 px-10 font-semibold ">
-      <h1>Places Layout</h1>
-      <nav className=" w-full flex relative justify-between items-center mx-auto px-8 h-16">
-        {/* logo */}
-        <div className="inline-flex">
-          <a className="_o6689fn">
-            <div className="hidden md:block">
-              <div className="inline-block mr-2 mt-2">
-                <button
-                onClick={isPlaceFormShow}
-                  type="button"
-                  className="focus:outline-none text-white text-sm py-2.5 px-5 rounded-md bg-gradient-to-r from-purple-400 to-purple-600 transform hover:scale-110"
-                >
-                  Thêm địa điểm
-                </button>
-              </div>
-              <button className="p-0 w-10 h-10 bg-white rounded-lg hover:bg-red-700 active:shadow-lg mouse shadow transition ease-in duration-200 focus:outline-none">
-                <svg
-                  viewBox="0 0 20 20"
-                  enableBackground="new 0 0 20 20"
-                  className="w-6 h-6 inline-block"
-                  onClick={isPlaceFormShow}
-                >
-                  <path
-                    fill="#999999"
-                    d="M16,10c0,0.553-0.048,1-0.601,1H11v4.399C11,15.951,10.553,16,10,16c-0.553,0-1-0.049-1-0.601V11H4.601
-                              C4.049,11,4,10.553,4,10c0-0.553,0.049-1,0.601-1H9V4.601C9,4.048,9.447,4,10,4c0.553,0,1,0.048,1,0.601V9h4.399
-                              C15.952,9,16,9.447,16,10z"
-                  />
-                </svg>
-              </button>
-            </div>
-            <div className="block md:hidden">
-              <svg
-                width={30}
-                height={32}
-                fill="currentcolor"
-                style={{ display: "block" }}
-              >
-                <path d="M29.24 22.68c-.16-.39-.31-.8-.47-1.15l-.74-1.67-.03-.03c-2.2-4.8-4.55-9.68-7.04-14.48l-.1-.2c-.25-.47-.5-.99-.76-1.47-.32-.57-.63-1.18-1.14-1.76a5.3 5.3 0 00-8.2 0c-.47.58-.82 1.19-1.14 1.76-.25.52-.5 1.03-.76 1.5l-.1.2c-2.45 4.8-4.84 9.68-7.04 14.48l-.06.06c-.22.52-.48 1.06-.73 1.64-.16.35-.32.73-.48 1.15a6.8 6.8 0 007.2 9.23 8.38 8.38 0 003.18-1.1c1.3-.73 2.55-1.79 3.95-3.32 1.4 1.53 2.68 2.59 3.95 3.33A8.38 8.38 0 0022.75 32a6.79 6.79 0 006.75-5.83 5.94 5.94 0 00-.26-3.5zm-14.36 1.66c-1.72-2.2-2.84-4.22-3.22-5.95a5.2 5.2 0 01-.1-1.96c.07-.51.26-.96.52-1.34.6-.87 1.65-1.41 2.8-1.41a3.3 3.3 0 012.8 1.4c.26.4.45.84.51 1.35.1.58.06 1.25-.1 1.96-.38 1.7-1.5 3.74-3.21 5.95zm12.74 1.48a4.76 4.76 0 01-2.9 3.75c-.76.32-1.6.41-2.42.32-.8-.1-1.6-.36-2.42-.84a15.64 15.64 0 01-3.63-3.1c2.1-2.6 3.37-4.97 3.85-7.08.23-1 .26-1.9.16-2.73a5.53 5.53 0 00-.86-2.2 5.36 5.36 0 00-4.49-2.28c-1.85 0-3.5.86-4.5 2.27a5.18 5.18 0 00-.85 2.21c-.13.84-.1 1.77.16 2.73.48 2.11 1.78 4.51 3.85 7.1a14.33 14.33 0 01-3.63 3.12c-.83.48-1.62.73-2.42.83a4.76 4.76 0 01-5.32-4.07c-.1-.8-.03-1.6.29-2.5.1-.32.25-.64.41-1.02.22-.52.48-1.06.73-1.6l.04-.07c2.16-4.77 4.52-9.64 6.97-14.41l.1-.2c.25-.48.5-.99.76-1.47.26-.51.54-1 .9-1.4a3.32 3.32 0 015.09 0c.35.4.64.89.9 1.4.25.48.5 1 .76 1.47l.1.2c2.44 4.77 4.8 9.64 7 14.41l.03.03c.26.52.48 1.1.73 1.6.16.39.32.7.42 1.03.19.9.29 1.7.19 2.5z" />
-              </svg>
-            </div>
-          </a>
-        </div>
-        {/* end logo */}
-        {/* search bar */}
-        <div className="hidden sm:block flex-shrink flex-grow-0 justify-start px-2">
-          <div className="inline-block text-gray-100	">
-            Showing 1 to 10 of {places.length} place
-          </div>
-        </div>
-        {/* end search bar */}
-        {/* login */}
-        <div className="flex-initial">
-          <div className="inline-flex items-center max-w-full ">
-            <div className="relative text-gray-600">
-              <input
-                type="search"
-                name="serch"
-                placeholder="Search"
-                className="bg-white h-10 px-5 pr-10 rounded-full text-sm focus:outline-none"
-              />
-              <button
-                type="submit"
-                className="absolute right-0 top-0 mt-3 mr-4"
-              >
-                <svg
-                  className="h-4 w-4 fill-current"
-                  xmlns="http://www.w3.org/2000/svg"
-                  xmlnsXlink="http://www.w3.org/1999/xlink"
-                  version="1.1"
-                  id="Capa_1"
-                  x="0px"
-                  y="0px"
-                  viewBox="0 0 56.966 56.966"
-                  style={{ enableBackground: "new 0 0 56.966 56.966" }}
-                  xmlSpace="preserve"
-                  width="512px"
-                  height="512px"
-                >
-                  <path d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z" />
-                </svg>
-              </button>
-            </div>
-          </div>
-        </div>
-        {/* end login */}
-      </nav>
+    <>
+      <NavBar user={user}></NavBar>
+      <div className="flex flex-row pt-20 bg-gray-50 ">
+        {/**Sidebar */}
+        <Sidebar></Sidebar>
 
-      <section className="container mx-auto p-6 font-mono">
-        <div className="w-full mb-8 overflow-hidden rounded-lg shadow-lg">
-          <div className="w-full overflow-x-auto">
-            <table className="w-full">
-              <thead className="bg-gray-50">
-                <tr className="text-md font-medium tracking-wide table-row text-left text-gray-500 uppercase bg-gray-100 uppercase border-b border-gray-600">
-                  <th className="px-4 py-3 w-1/4 ">Name</th>
-                  <th className="px-4 py-3">Age</th>
-                  <th className="px-4 py-3 w-1/5">Status</th>
-                  <th className="px-4 py-3 text-center">Actions</th>
-                </tr>
-              </thead>
-              <tbody className="bg-white divide-y divide-gray-200 ">
-                {places.map((place, index) => (
-                  <tr
-                    key={place.id}
-                    className="text-gray-700 hover:bg-gray-100 hover:shadow-lg sm:table-row"
-                  >
-                    <td className="px-4 py-3 ">
-                      <div className="flex items-center text-sm">
-                        <div className="relative w-8 h-8 mr-3 rounded-full md:block"></div>
-                        <div>
-                          <p className="font-semibold   text-gray-100">
-                            {place.name}
-                          </p>
-                        </div>
-                      </div>
-                    </td>
-                    <td className="px-4 py-3  text-ms font-semibold ">
-                      <p className="line-clamp-2">{place.description}</p>
-                    </td>
-                    <td className="px-4 py-3 text-xs ">
-                      <span className="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-sm">
-                        {" "}
-                        {place.address}{" "}
-                      </span>
-                    </td>
+        {/**Content */}
 
-                    <td className="px-4 py-3 text-xs ">
-                      <div className="flex item-center justify-center">
-                        <div className="w-4 mr-2 transform hover:text-purple-500 hover:scale-110">
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                          >
-                            <path
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                              strokeWidth={2}
-                              d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                            />
-                            <path
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                              strokeWidth={2}
-                              d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
-                            />
-                          </svg>
-                        </div>
-                        <div className="w-4 mr-2 transform hover:text-purple-500 hover:scale-110">
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                          >
-                            <path
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                              strokeWidth={2}
-                              d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
-                            />
-                          </svg>
-                        </div>
-                        <div className="w-4 mr-2 transform hover:text-purple-500 hover:scale-110">
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                          >
-                            <path
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                              strokeWidth={2}
-                              d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-                            />
-                          </svg>
-                        </div>
-                      </div>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-        </div>
-      </section>
-      <PlaceForm
-        visible={isShowPlaceForm}
-        isPlaceFormShow={isPlaceFormShow}
-      ></PlaceForm>
-    </div>
+        <AddPlace
+          total={total}
+          places={places}
+          setPlaces={setPlaces}
+          searchText={searchText}
+          setSearchText={setSearchText}
+        ></AddPlace>
+      </div>
+    </>
   );
 }
